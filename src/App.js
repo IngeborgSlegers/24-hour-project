ab270d5f7e11fd871ed16648142cc002944e699d
Jerome Martin Flaherty <jerbear2@gmail.com>
2020-09-09 13:09:40 -0400

    fetch working

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Zomato from './components/Zomato/Zomato';
 
 function App() {
-  const [latitude, setLatitude] = React.useState('')
-  const [longitude, setLongitude] = React.useState('')
+  const [latitude, setLatitude] = React.useState()
+  const [longitude, setLongitude] = React.useState()
   React.useEffect(() => {
-    navigator.geolocation.getCurrentPosition(location => { setLatitude(location.coords.latitude); setLongitude(location.coords.longitude) }, fail => console.log('you arent real'))
+    (async function iifeMan() {
+      await navigator.geolocation.getCurrentPosition(location => { setLatitude(location.coords.latitude); setLongitude(location.coords.longitude) }, fail => console.log('you arent real'))
+    })();
   }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+
+      <Zomato lat={latitude} long={longitude} />
     </div>
   );
 }
--- src/components/Zomato/Zomato.jsx
+++ src/components/Zomato/Zomato.jsx
@@ -1,11 +1,52 @@
 import React from 'react'
 
-const Zomato = () => {
+const Zomato = (props) => {
+  const [restaurants, setRestaurants] = React.useState([])
+
+  React.useEffect(() => {
+    console.log(props.lat, props.long)
+    console.log(window.location)
+    if (props.lat !== undefined && props.long !== undefined) {
+      console.log('RUNNNING FETCH')
+      fetch(`https://developers.zomato.com/api/v2.1/geocode?lat=${props.lat}&lon=${props.long}`, {
+        method: 'GET',
+        headers: new Headers({
+          'Content-Type': 'application/json',
+          'user-key': '802cf8c5fd3a11e515e11f7e1cd70b50',
+          // 'lat': props.lat,
+          // 'lon': props.long
+        })
+      })
+        .then(res => res.json())
+        .then(json => setRestaurants(json.nearby_restaurants))
+        .catch(err => console.log(err))
+    }
+  }, [props.lat, props.long])
   return (
     <div>
-      
+      {
+        restaurants.map((location, index) => {
+          return (
+            <div className="location-card" key={index}>
+              {location.restaurant.name}
+            </div>
+          )
+        })
+      }
     </div>
   )
 }
-
 export default Zomato
+
+
+// fetch('https://developers.zomato.com/api/v2.1/categories', {
+//       method: 'GET',
+//       headers: new Headers({
+//         'user-key': '802cf8c5fd3a11e515e11f7e1cd70b50',
+//         'lat': props.lat,
+//         'lon': props.long
+//       })
+//     })
+//     .then(res => res.json())
+//     .then(json => console.log(json))
+//     .catch(err => console.log(err))
