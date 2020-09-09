063eee415dcffc9a650a1de390f070456f4a33de
Ingeborg Slegers <ingeborgfreda@gmail.com>
2020-09-09 16:50:06 -0400

    got the nasa img to store and badly display

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
+import NASA from './components/NASA/NASA';
+
 import './App.css';
 
 function App() {
@@ -7,23 +8,10 @@ function App() {
   const [longitude, setLongitude] = React.useState('')
   React.useEffect(() => {
     navigator.geolocation.getCurrentPosition(location => { setLatitude(location.coords.latitude); setLongitude(location.coords.longitude) }, fail => console.log('you arent real'))
-  }, [])
+  }, [latitude, longitude])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NASA lat={latitude} long={longitude}/>
     </div>
   );
 }
