d1cdac413ad094441d94dc17d66f44d0cad8c719
Donovan Triplett <donotriplett@gmail.com>
2020-09-10 16:53:45 -0400

    added the ability to switch units

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -5,30 +5,45 @@ const Weather = (props) => {
     `api.openweathermap.org/data/2.5/weather`
   );
   const [weather, setWeather] = useState(undefined);
+  const [units, setUnits] = useState("imperial");
   const apiKey = "40f89b7a717e3d03adafe2918d6d4bc2";
 
   useEffect(() => {
     const fetchWeather = async () => {
       const response = await fetch(
-        `http://${fetchURL}?lat=${props.lat}&lon=${props.long}&appid=${apiKey}&units=metric`
+        `http://${fetchURL}?lat=${props.lat}&lon=${props.long}&appid=${apiKey}&units=${units}`
       );
       const json = await response.json();
       setWeather(json);
     };
     fetchWeather();
-  }, []);
+  }, [units]);
+
+  const switchUnits = () => {
+    if (units === "metric"){
+      setUnits("imperial");
+    } else {
+      setUnits("metric");
+    }
+  }
 
   return (
     <div>
-      {weather ? (
+      {weather && units === "metric" ? (
         <div>
           <h1>{weather.main.temp}&#8451;</h1>
           <h1>High: {weather.main.temp_max}&#8451;</h1>
           <h1>Low: {weather.main.temp_min}&#8451;</h1>
+          <button onClick={switchUnits}>Switch Units</button>
         </div>
-      ) : (
-        <h1>Loading...</h1>
-      )}
+      ) : weather && units === "imperial" ? (
+        <div>
+          <h1>{weather.main.temp}&#8451;</h1>
+          <h1>High: {weather.main.temp_max}&#8451;</h1>
+          <h1>Low: {weather.main.temp_min}&#8451;</h1>
+          <button onClick={switchUnits}>Switch Units</button>
+        </div>
+      ) : <h1>Loading...</h1>}
     </div>
   );
 };
