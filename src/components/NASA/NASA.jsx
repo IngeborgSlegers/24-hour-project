978b3e314d0c8718371450f41c0f3b02112a8fd8
Ingeborg Slegers <ingeborgfreda@gmail.com>
2020-09-10 09:51:56 -0400

    nasa card is centered

--- src/components/NASA/NASA.jsx
+++ src/components/NASA/NASA.jsx
@@ -1,19 +1,38 @@
 import React, { useState, useEffect } from "react";
-import { CardMedia, Card } from "@material-ui/core";
-import Alert from "@material-ui/lab/Alert"
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  CardMedia,
+  Card,
+  CardActionArea,
+  Typography,
+  Box,
+} from "@material-ui/core";
+import Alert from "@material-ui/lab/Alert";
+
+const useStyles = makeStyles({
+  media: {
+    height: 345,
+  },
+  card: {
+    maxWidth: 345,
+    border: "1px black solid",
+    margin: "auto",
+  },
+});
 
 const NASA = (props) => {
+  const classes = useStyles();
+
   const [NASA_URL, setNASA_URL] = useState(
     "https://api.nasa.gov/planetary/earth/imagery"
   );
   const [key, setKey] = useState("aUYcj0GHGEp6FNA9OhPtKilnflH5WheAt9KlfozB");
   const [NASA_Img, setNASA_Img] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-  // console.log(NASA_Img);
+  const [errorMessage, setErrorMessage] = useState(" ");
 
-  const toInfinityAndBeyond = async () => {
+  const toInfinityAndBeyond = () => {
     fetch(
-      `${NASA_URL}?lon=${props.long}&lat=${props.lat}&api_key=${key}&date=2020-01-01`
+      `${NASA_URL}?lon=${props.long}&lat=${props.lat}&api_key=${key}&date=2020-09-01`
     )
       .then((res) => {
         if (!res.ok) {
@@ -36,19 +55,21 @@ const NASA = (props) => {
   }, [props.lat, props.long]);
 
   return (
-    <div>
-      {errorMessage ? (
-        <Card>
+    <Box className={classes.root}>
+      <Typography variant="h4">NASA</Typography>
+      {errorMessage.length !== 0 || NASA_Img.length !== 0 ? (
+        <Card className={classes.card}>
           <CardMedia
+            className={classes.media}
             image={NASA_Img}
             src={NASA_Img}
             title="no image available"
           />
         </Card>
       ) : (
-        <Alert>${errorMessage}</Alert>
+        <Alert severity="error">Oops {errorMessage}</Alert>
       )}
-    </div>
+    </Box>
   );
 };
 
