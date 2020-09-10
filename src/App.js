1f2fe2585448004b731d3351e46b929bf0eba444
Donovan Triplett <donotriplett@gmail.com>
2020-09-10 15:32:51 -0400

    Showing temperatures

--- src/App.js
+++ src/App.js
@@ -3,18 +3,28 @@ import NASA from './components/NASA/NASA';
 
 import './App.css';
 import Zomato from './components/Zomato/Zomato';
+import Weather from './components/Weather/Weather';
 
 function App() {
   const [latitude, setLatitude] = React.useState()
   const [longitude, setLongitude] = React.useState()
   React.useEffect(() => {
 
-    navigator.geolocation.getCurrentPosition(location => { setLatitude(location.coords.latitude); setLongitude(location.coords.longitude) }, fail => console.log('you arent real'))
+    navigator.geolocation.getCurrentPosition(location => { setLatitude(location.coords.latitude.toFixed(2)); setLongitude(location.coords.longitude.toFixed(2)) }, fail => console.log('you arent real'))
   }, [latitude, longitude])
   return (
     <div className="App">
-      <NASA lat={latitude} long={longitude}/>
-      <Zomato lat={latitude} long={longitude} />
+      {
+        latitude && longitude 
+        ? 
+        <div>
+        <NASA lat={latitude} long={longitude}/>
+        <Zomato lat={latitude} long={longitude} />
+        <Weather lat={latitude} long={longitude} />
+        </div>
+        :
+        <></>
+      }
     </div>
   );
 }
